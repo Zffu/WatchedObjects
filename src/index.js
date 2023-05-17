37dbcb4d1b5be6b5ffc920091a4633aab59e565e
Zffu <103074097+Zffu@users.noreply.github.com>
2023-05-17 11:32:29 +0200

    Moved the uuid making function to the WatchedObjectsRegistry object

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 const WatchedObjectsRegistry = {
-	watchingObjects: {}
+	watchingObjects: {},
+	makeUUID: function() {
+		return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
+    		(c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+  		);
+	}
 }
 
 class StoredWatchedObject {
@@ -9,7 +14,7 @@ class StoredWatchedObject {
 		this.lastTimeValueUpdated = new Date().getTime()
 		this.storedValue = value;
 		this.oldValues = [];
-		this.watchingUUID = uuidv4()
+		this.watchingUUID = WatchedObjectsRegistry.makeUUID()
 		WatchedObjectsRegistry.watchingObjects[this.watchingUUID] = this;
 	}
 }
@@ -55,9 +60,4 @@ class WatchedObject {
 	}
 }
 
-function uuidv4() {
-  return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
-    (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
-  );
-}
 
